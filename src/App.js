87a4d90343cf344226d8747bb226fc46483a6e08
Nurhadi Permana <hadistylecool@gmail.com>
2023-05-09 14:17:27 +0700

    Counter App Redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import Form from "./Materi/Form";
+// import Hooks from "./Materi/Hooks";
+// import './App.css';
+import Redux from './Materi/Redux';
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      <Redux />
     </div>
   );
 }
 
 export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Belajar React Routing</h1>
+
+//       <nav>
+//         <Link to="/home" className="mr">
+//           Home
+//         </Link>
+//         <Link to="/about" className="mr">
+//           About
+//         </Link>
+//         <Link to="/category/makanan" className="mr">
+//           Makanan
+//         </Link>
+//         <Link to="/category/minuman" className="mr">
+//           Minuman
+//         </Link>
+//       </nav>
+
+//       <Outlet />
+//     </div>
+//   );
+// }
+
+// export default App;
